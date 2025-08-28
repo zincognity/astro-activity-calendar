1921bfde5cbb5688f776da0b1de8a787414081ee
zincognity <67128655+zincognity@users.noreply.github.com>
2025-08-28 17:34:14 -0500

    fix: streamline package.json formatting and add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx}",
+    "./node_modules/astro-github-activity/**/*.{astro,js,ts}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
